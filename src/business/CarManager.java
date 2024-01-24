ee35c3060d288e37451ac65cc49adfdb429e518b
Devran EROĞLU <devraneroglu@gmail.com>
2024-01-25 02:29:28 +0300

    Available for rent

--- src/business/CarManager.java
+++ src/business/CarManager.java
@@ -1,17 +1,23 @@
 package business;
 
 import core.Helper;
+import dao.BookDao;
 import dao.CarDao;
+import entitiy.Book;
 import entitiy.Car;
 import entitiy.Model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CarManager {
     public final CarDao carDao;
+    public final BookDao bookDao;
 
     public CarManager() {
         this.carDao = new CarDao();
+        this.bookDao = new BookDao();
     }
 
     public Car getById(int id) {
@@ -64,6 +70,52 @@ public class CarManager {
             return false;
         }
         return this.carDao.delete(id);
+    }
+
+    public ArrayList<Car> searchByBooking(String start_date, String finish_date, Model.Type type, Model.Gear gear, Model.Fuel fuel) {
+        String query = "SELECT * FROM public.car LEFT JOIN public.model  ";
+        ArrayList<String> where = new ArrayList<>();
+        ArrayList<String> joinWhere = new ArrayList<>();
+        ArrayList<String> bookOrWhere = new ArrayList<>();
+
+        joinWhere.add("public.car.car_model_id = public.model.model_id");
+
+        start_date = LocalDate.parse(start_date, DateTimeFormatter.ofPattern("dd/MM/yyyy")).toString();
+        finish_date = LocalDate.parse(finish_date, DateTimeFormatter.ofPattern("dd/MM/yyyy")).toString();
+
+
+        if (gear != null) where.add("model.model_gear = '" + gear.toString() + "' ");
+        if (fuel != null) where.add("model.model_fuel = '" + fuel.toString() + "' ");
+        if (type != null) where.add("model.model_type = '" + type.toString() + "' ");
+
+        String whereStr = String.join(" AND ", where);
+        String joinStr = String.join(" AND ", joinWhere);
+        if (joinStr.length() > 0) query += " ON " + joinStr;
+        if (whereStr.length() > 0) query += " WHERE " + whereStr;
+
+        ArrayList<Car> searchedCarList = this.carDao.selectByQuery(query);
+        bookOrWhere.add(" ( '" + start_date + "' BETWEEN book_str_date AND book_fnsh_date  ) ");
+        bookOrWhere.add(" ( '" + finish_date + "' BETWEEN book_str_date AND book_fnsh_date  ) ");
+        bookOrWhere.add(" ( book_str_date BETWEEN '" + start_date + "' AND '" + finish_date + "' ) ");
+        bookOrWhere.add(" ( book_fnsh_date BETWEEN '" + start_date + "' AND '" + finish_date + "' ) ");
+
+        String bookOrWhereStr = String.join(" OR ", bookOrWhere);
+        String bookQuery = "SELECT * FROM public.book WHERE " + bookOrWhereStr;
+
+        ArrayList<Book> busyCarList = this.bookDao.selectByQuery(bookQuery);
+        ArrayList<Integer> busyCarID = new ArrayList<>();
+        for (Book book : busyCarList) {
+            busyCarID.add(book.getCar_id());
+        }
+        searchedCarList.removeIf(car -> busyCarID.contains(car.getId()));
+
+        // System.out.println(busyCarList);
+        // System.out.println(bookQuery);
+
+        //    System.out.println(query);
+        //    return new ArrayList<>();
+        //  return this.carDao.selectByQuery(query);
+        return searchedCarList;
     }
 
 }
--- src/view/AdminView.java
+++ src/view/AdminView.java
@@ -1,5 +1,6 @@
 package view;
 
+import business.BookManager;
 import business.BrandManager;
 import business.CarManager;
 import business.ModelManager;
@@ -12,8 +13,12 @@ import entitiy.User;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.text.MaskFormatter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 public class AdminView extends Layaout {
@@ -21,7 +26,6 @@ public class AdminView extends Layaout {
     private JLabel lbl_welcome;
     private JPanel pnl_top;
     private JTabbedPane tab_menu;
-    private JButton btn_logout;
     private JPanel pnl_brand;
     private JScrollPane scrl_brand;
     private JTable tbl_brand;
@@ -37,23 +41,44 @@ public class AdminView extends Layaout {
     private JPanel pnl_car;
     private JScrollPane scrl_car;
     private JTable tbl_car;
+    private JPanel pnl_booking;
+    private JScrollPane scrl_booking;
+    private JPanel pnl_booking_search;
+    private JTextField fld_begdate;
+    private JTextField fld_enddate;
+    private JComboBox cmb_booking_type;
+    private JComboBox cmb_booking_fuel;
+    private JLabel lbl_begdate;
+    private JLabel lbl_enddate;
+    private JComboBox cmb_booking_gear;
+    private JButton btn_booking_save;
+    private JTable tbl_booking;
+    private JLabel lbl_booking_type;
+    private JLabel lbl_booking_fuel;
+    private JLabel lbl_booking_gear;
+    private JButton btn_booking_delete;
     private User user;
     private DefaultTableModel tmdl_brand = new DefaultTableModel();
     private DefaultTableModel tmdl_model = new DefaultTableModel();
     private DefaultTableModel tmdl_car = new DefaultTableModel();
+    private DefaultTableModel tmdl_booking = new DefaultTableModel();
     private BrandManager brandManager;
     private ModelManager modelManager;
     private CarManager carManager;
+    private BookManager bookManager;
     private JPopupMenu brand_menu;
     private JPopupMenu model_menu;
     private JPopupMenu car_menu;
+    private JPopupMenu booking_menu;
     private Object[] col_model;
     private Object[] col_car;
+
 
     public AdminView(User user) {
         this.modelManager = new ModelManager();
         this.brandManager = new BrandManager();
         this.carManager = new CarManager();
+        this.modelManager = new ModelManager();
         this.add(container);
         this.initilizeGui(1000, 500);
         this.user = user;
@@ -73,6 +98,24 @@ public class AdminView extends Layaout {
         loadCarTable();
         loadCarCompanent();
 
+        loadBookingTable(null);
+        loadBookingCompanent();
+        loadBookingFilter();
+
+        btn_booking_save.addActionListener(e -> {
+            ArrayList<Car> carList = this.carManager.searchByBooking(
+                    fld_begdate.getText(),
+                    fld_enddate.getText(),
+                    (Model.Type) cmb_booking_type.getSelectedItem(),
+                    (Model.Gear) cmb_booking_gear.getSelectedItem(),
+                    (Model.Fuel) cmb_booking_fuel.getSelectedItem()
+            );
+            ArrayList<Object[]> carBookingRow = this.carManager.getForTable(this.col_car.length, carList);
+            loadBookingTable(carBookingRow);
+        });
+        btn_booking_delete.addActionListener(e -> {
+            loadBookingFilter();
+        });
     }
 
     public void loadBrandCompanent() {
@@ -237,7 +280,7 @@ public class AdminView extends Layaout {
     }
 
     public void loadCarTable() {
-        Object[] col_car = {"ID", "Marka", "Model", "Plaka", "Renk", "KM", "Yıl", "Tip", "Yakıt", "Vites"};
+        col_car = new Object[]{"ID", "Marka", "Model", "Plaka", "Renk", "KM", "Yıl", "Tip", "Yakıt", "Vites"};
         ArrayList<Object[]> carList = this.carManager.getForTable(col_car.length, this.carManager.findAll());
         this.createTable(this.tmdl_car, this.tbl_car, col_car, carList);
     }
@@ -283,5 +326,33 @@ public class AdminView extends Layaout {
 
     }
 
+    public void loadBookingTable(ArrayList<Object[]> carList) {
+        Object[] col_booking_list = {"ID", "Marka", "Model", "Plaka", "Renk", "KM", "Yıl", "Tip", "Yakıt", "Vites"};
+        this.createTable(this.tmdl_booking, this.tbl_booking, col_booking_list, carList);
+    }
 
+    public void loadBookingCompanent() {
+        tableRowSelect(this.tbl_booking);
+        this.booking_menu = new JPopupMenu();
+        booking_menu.add("Rezervasyon yap").addActionListener(e -> {
+        });
+        this.tbl_booking.setComponentPopupMenu(booking_menu);
+    }
+
+    public void loadBookingFilter() {
+
+        this.cmb_booking_gear.setModel(new DefaultComboBoxModel<>(Model.Gear.values()));
+        this.cmb_booking_gear.setSelectedItem(null);
+        this.cmb_booking_fuel.setModel(new DefaultComboBoxModel<>(Model.Fuel.values()));
+        this.cmb_booking_fuel.setSelectedItem(null);
+        this.cmb_booking_type.setModel(new DefaultComboBoxModel<>(Model.Type.values()));
+        this.cmb_booking_type.setSelectedItem(null);
+    }
+
+    private void createUIComponents() throws ParseException {
+        this.fld_begdate = new JFormattedTextField(new MaskFormatter("##/##/####"));
+        this.fld_begdate.setText("01/01/2024");
+        this.fld_enddate = new JFormattedTextField(new MaskFormatter("##/##/####"));
+        this.fld_enddate.setText("05/05/2024");
+    }
 }
